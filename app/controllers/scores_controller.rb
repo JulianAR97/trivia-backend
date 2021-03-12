d316c8ff1e3b7d96184f30c37ba8c58fb1cfc625
Julian <julianrosenthal@yahoo.com>
2021-03-12 15:15:43 -0700

    only render name and count

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,6 +1,6 @@
 class ScoresController < ApplicationController
   def index
     @scores = Score.get_top_ten_scores
-    render json: @scores
+    render json: @scores, only: [:name, :count]
   end
 end
