7c5af4fc8f5277ce2d6e02437796ff1d1705796c
Julian <julianrosenthal@yahoo.com>
2021-03-12 17:03:56 -0700

    added score params and create method

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -3,4 +3,15 @@ class ScoresController < ApplicationController
     @scores = Score.get_top_ten_scores
     render json: @scores, only: [:name, :count]
   end
+
+  def create
+    Score.create(score_params)
+  end
+
+
+  private
+
+  def score_params
+    params.require(:score).permit([:name, :count])
+  end
 end
