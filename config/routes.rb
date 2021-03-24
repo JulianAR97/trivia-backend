25b5f2ce9e4c74d637ced58aae9702267ac6b07d
Julian <julianrosenthal@yahoo.com>
2021-03-23 18:42:58 -0700

    added nested route for filtering by category

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/scores' => 'scores#index'
+  
+  
+  get '/:category/scores' => 'scores#index'
+
+
   post '/scores' => 'scores#create'
 end
