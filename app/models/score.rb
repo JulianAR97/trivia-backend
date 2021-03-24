4102b34c33024323a6a9b647873a24f4b6e16c2d
Julian <julianrosenthal@yahoo.com>
2021-03-23 18:43:45 -0700

    added method to slug name

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,3 +1,9 @@
 class Score < ApplicationRecord
   scope :get_top_ten_scores, -> { order('count desc').limit(10) }
+
+  belongs_to :category
+
+  def self.slug_to_name(slug)
+    slug == 't-v' ? 'T.V.' : slug.split('-').join(' ').titlecase
+  end
 end
